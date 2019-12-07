16292f5f2025b9c333f0be012ee20e57127c28a7
tovarb <ybere_tovar@hotmail.com>
2019-12-07 00:07:34 -0600

    Navigation component is added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from './Navigation/Navigation';
+
+class App extends Component {
+  /*
+  constructor(){
+    super();
+
+  }*/
+
+  render(){
+    return(
+      <div>
+        <Navigation />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Navigation/Navigation.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import './Navigation.css';
+
+import logo from '../MercadoLibre_logo.png';
+import promo from '../disc-v.png';
+import location from '../location.png';
+import shoppingCart from '../shopping-cart.png';
+
+class Navigation extends Component {
+    /*
+    constructor(props){
+        super(props);
+    }*/
+
+
+    render() {
+        return(
+            <div id="navContainer">
+                <nav className="navbar navbar-expand-lg navbar-light">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                        <a className="navbar-brand" href="https://www.mercadolibre.com.mx/"><img id="logo" src={logo} alt="logo-mercadopago" /></a>
+                        <div className="input-group mb-1">
+                            <input type="text" className="form-control input-lg" placeholder="Buscar productos, marcas y más..." aria-label="Recipient's username" aria-describedby="button-addon2" />
+                            <div className="input-group-append">
+                                <button className="btn btn-light" type="button"><i className="fas fa-search"></i></button>
+                            </div>
+                        </div>
+                        <div className="input-group ">
+                            <img id="promo" src={promo} alt="logo-mercadopago" />
+                        </div>
+                    </div>
+                </nav>
+                <div className="navbar navbar-expand-lg navbar-light mb-1">
+                    <img  src={location} alt="location-icon" />
+                    <a className="navbar-brand postal-c" href="https://www.mercadolibre.com.mx/">
+                        <span>Ingresa tu</span> <br/>código postal
+                    </a>
+                    <div className="input-group" id="navbarText">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <a className="nav-link" href="https://www.mercadolibre.com.mx/">Categorías</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="https://www.mercadolibre.com.mx/">Ofertas</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="https://www.mercadolibre.com.mx/">Historial</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="https://www.mercadolibre.com.mx/">Vender</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="https://www.mercadolibre.com.mx/">Ayuda</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="input-group" id="navbarText">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <a className="nav-link" href="https://www.mercadolibre.com.mx/">Crear tu cuenta</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="https://www.mercadolibre.com.mx/">Ingresa</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="https://www.mercadolibre.com.mx/">Mis compras</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="https://www.mercadolibre.com.mx/"><img src={shoppingCart} alt="shoppping-cart" /></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Navigation;
